84416c54773a80f1e2efa46aa92cfd2a3f531199
Tajveer Nijjar <Tajveer.Nijjar@connexionz.com>
2021-04-09 09:49:02 +1200

    Preferences are running.

--- MainActivity.cs
+++ MainActivity.cs
@@ -19,42 +19,12 @@ namespace PreferencesTry
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            Title = "Preference Try";
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
-        }
-
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
-            {
-                return true;
-            }
-
-            return base.OnOptionsItemSelected(item);
-        }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (View.IOnClickListener)null).Show();
-        }
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            SupportFragmentManager
+                .BeginTransaction()
+                .Replace(Resource.Id.settings_container, new SettingsFragment())
+                .Commit();
         }
 	}
 }
---
+++ SettingsFragment.cs
@@ -0,0 +1,14 @@
+﻿using Android.OS;
+using AndroidX.Fragment.App;
+using AndroidX.Preference;
+
+namespace PreferencesTry
+{
+    internal class SettingsFragment : PreferenceFragmentCompat
+    {
+        public override void OnCreatePreferences(Bundle savedInstanceState, string rootKey)
+        {
+            SetPreferencesFromResource(Resource.Xml.preferences, rootKey);
+        }
+    }
+}
