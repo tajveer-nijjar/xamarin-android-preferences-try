bf55622c8043c8311c00cf4d5963a23d8dfc57b8
Tajveer Nijjar <Tajveer.Nijjar@connexionz.com>
2021-04-09 10:08:17 +1200

    Preferences working.

--- MainActivity.cs
+++ MainActivity.cs
@@ -7,12 +7,17 @@ using AndroidX.AppCompat.Widget;
 using AndroidX.AppCompat.App;
 using Google.Android.Material.FloatingActionButton;
 using Google.Android.Material.Snackbar;
+using Xamarin.Essentials;
+using static PreferencesTry.Settings;
+using Android.Widget;
 
 namespace PreferencesTry
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private Button _getAllSettingsButton;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -25,6 +30,16 @@ namespace PreferencesTry
                 .BeginTransaction()
                 .Replace(Resource.Id.settings_container, new SettingsFragment())
                 .Commit();
+
+            _getAllSettingsButton = FindViewById<Button>(Resource.Id.getAllSettingsButton);
+
+            _getAllSettingsButton.Click += GetAllSettingsButton_Click;
         }
-	}
+
+        private void GetAllSettingsButton_Click(object sender, EventArgs e)
+        {
+            var x = IsMediusInstalled;
+            var y = CompanyTag;
+        }
+    }
 }
---
+++ Settings.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Xamarin.Essentials;
+
+namespace PreferencesTry
+{
+    public class Settings
+    {
+        public static bool IsMediusInstalled
+        {
+            get 
+            { 
+                return Preferences.Get(Application.Context.GetString(Resource.String.has_medius_installed_key), true);
+            }
+            //set { myVar = value; }
+        }
+
+        public static string CompanyTag
+        {
+            get
+            {
+                return Preferences.Get(Application.Context.GetString(Resource.String.company_tag_key), "10");
+            }
+        }
+    }
+}
